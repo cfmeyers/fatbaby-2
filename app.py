c71e2ea602d127f04db1fc085fc60bb0e0e978c5
Collin Meyers <cfmeyers@gmail.com>
2022-12-07 23:15:35 -0500

    Do not use google sheets data

--- app.py
+++ app.py
@@ -9,5 +9,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    next_feeding = get_next_feeding_time()
+    # next_feeding = get_next_feeding_time()
+    next_feeding = 11
     return render_template("index.html", next_feeding=next_feeding)
