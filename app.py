ef927c41f3898e8ab33320af7f9853742ecc4f8b
Collin Meyers <cfmeyers@gmail.com>
2022-12-07 22:23:09 -0500

    Add template

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from random import randint
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -5,4 +7,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    next_feeding = randint(0, 10)
+    return render_template("index.html", next_feeding=next_feeding)
