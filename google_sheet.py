a56bd664af51c26efa9368c926328a678e277ae8
Collin Meyers <cfmeyers@gmail.com>
2023-03-09 15:17:27 -0500

    Add most recent feed amount

--- google_sheet.py
+++ google_sheet.py
@@ -23,7 +23,7 @@ class BabyEvent(NamedTuple):
 
     created_at: datetime
     diaper_type: str
-    formula_amount: Optional[float]
+    formula_amount: float | None
     people: list[str]
 
 
@@ -37,6 +37,7 @@ class BabyUpdate(NamedTuple):
     most_recent_feed: datetime
     next_feed: datetime
     most_recent_dirty_diaper: datetime
+    most_recent_feed_amount: float | None
 
     @property
     def pretty_most_recent_feed(self) -> str:
@@ -53,7 +54,7 @@ class BabyUpdate(NamedTuple):
 
 def get_sheet() -> gspread.worksheet.Worksheet:
     """Get the Google Sheet that holds the raw baby event data"""
-    credentials = json.loads(GOOGLE_CREDENTIALS)
+    credentials = json.loads(GOOGLE_CREDENTIALS)  # type:ignore
 
     gc = gspread.service_account_from_dict(credentials)
     return gc.open(SHEET_NAME).sheet1
@@ -89,9 +90,9 @@ def get_baby_events_from_sheet(sheet: gspread.worksheet.Worksheet) -> list[BabyE
     return [parse_baby_event_from_record(v) for v in vals[1:]]
 
 
-def get_most_recent_feeding_time_from_events(events: list[BabyEvent]) -> datetime:
+def get_most_recent_feed_event(events: list[BabyEvent]) -> BabyEvent:
     most_recent = [e for e in events if e.formula_amount is not None][-1]
-    return most_recent.created_at
+    return most_recent
 
 
 def get_most_recent_dirty_diaper_from_events(events: list[BabyEvent]) -> datetime:
@@ -102,19 +103,21 @@ def get_most_recent_dirty_diaper_from_events(events: list[BabyEvent]) -> datetim
 def get_next_feeding_time_from_events(
     events: list[BabyEvent], delta: float = 3.0
 ) -> datetime:
-    last_feeding_time = get_most_recent_feeding_time_from_events(events)
-    return last_feeding_time + timedelta(hours=delta)
+    most_recent_event = get_most_recent_feed_event(events)
+    return most_recent_event.created_at + timedelta(hours=delta)
 
 
 def get_baby_update() -> BabyUpdate:
     """Get most recent state of baby feeding/diapers from Google Sheet"""
     sheet = get_sheet()
     events = get_baby_events_from_sheet(sheet)
-    most_recent_feed = get_most_recent_feeding_time_from_events(events)
+    most_recent_feed = get_most_recent_feed_event(events)
+    most_recent_feed_amount = most_recent_feed.formula_amount
     next_feed = get_next_feeding_time_from_events(events, delta=3.0)
     most_recent_dirty_diaper = get_most_recent_dirty_diaper_from_events(events)
     return BabyUpdate(
-        most_recent_feed=most_recent_feed,
+        most_recent_feed=most_recent_feed.created_at,
         next_feed=next_feed,
         most_recent_dirty_diaper=most_recent_dirty_diaper,
+        most_recent_feed_amount=most_recent_feed_amount,
     )
